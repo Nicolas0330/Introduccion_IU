25394b7799f7762c40bbceffe95a9ee009778b97
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 13:36:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 
-st.title(Aplicaviones para Ciudades Inteligentes")
+st.title("Aplicaviones para Ciudades Inteligentes")
 
 st.header("En este espacio podras obtener informacion de tu ciudad")
 
