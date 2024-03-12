c8c56c2440eb08879711303d53000fe58a322194
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 13:28:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,4 +6,5 @@ st.title(Aplicaviones para Ciudades Inteligentes")
 # st.header("En este espacio podras obtener informacion de tu ciudad")
 
 # image = Image.open('Sistemaiot.png')
-# st.
+# st.image(image, caption='Inteligencia Urbana')
+
