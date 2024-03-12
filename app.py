c9ba9ec789593615e06f031ad76f4a877e542a45
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 13:39:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from PIL import Image
 
 st.title("Aplicaviones para Ciudades Inteligentes")
 
-st.header("En este espacio podras obtener informacion de tu ciudad")
+st.header("En este espacio podras obtener informacion sobre el patrimonio cultural de tu ciudad")
 
 image = Image.open('Escultura.jpg')
 st.image(image, caption='Inteligencia Urbana')
 # st.write("Enlace para mi sistema de internet de las cosas")
-
+# st.write("Ingresa al link")
