ade88173f1920b596dbb48b84a48237cb12f772a
Nicolas0330 <163177274+Nicolas0330@users.noreply.github.com>
2024-03-12 13:22:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
+import streamlit as st
+from PIL import Image
 
+st.title(Aplicaviones para Ciudades Inteligentes")
+
+# st.header("En este espacio podras obtener informacion de tu ciudad")
+
+# image = Image.open('Sistemaiot.png')
+# st.
